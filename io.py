57a815731cbcd4dfc190128422a0b72f237662ce
budzej <rudolf13.00@wp.pl>
2023-09-14 10:31:12 +0200

    kalkulator io2.py

--- io.py
+++ io.py
@@ -9,3 +9,17 @@ Trzecia linijka
 print("Jakiś tekst")#funkcja wyjścia
 print ("Kolejny tekst") 
 print("Tekst dodany w Visual") #tekst
+
+a = 3
+b = 3.
+c = "Coś tam"
+d = [3, 5, 'hhh']
+
+print (type (a))
+print (type (b))
+print (type (c))
+print (type (d))
+
+b = int(b)
+print(type(b))
+print(b)
