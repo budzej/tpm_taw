0f1ae272b248c8d32ced19cc119338042dd97912
budzej <rudolf13.00@wp.pl>
2023-09-14 10:31:39 +0200

    kalkulator io2.py

---
+++ io2.py
@@ -0,0 +1,14 @@
+'''
+Prosty kalkulator
+Python, ver. 3.11
+B>R 2023
+'''
+
+x = float(input("Podaj x= "))
+y = float(input("Podaj y= "))
+
+#obliczenia
+suma = x + y
+roznica = x - y
+iloczyn = x * y
+iloraz = x / y
