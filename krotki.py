d75b9455df746c1125f42b5126c003f435aa9aad
budzej <rudolf13.00@wp.pl>
2023-09-21 10:30:46 +0200

    Tuple

---
+++ krotki.py
@@ -0,0 +1,25 @@
+'''
+Typ sekwencyjny krotki - immutable!!!!!!
+TUPLA
+'''
+
+# moja_tupla = (3,4,5)
+# print(type(moja_tupla))
+# print(moja_tupla)
+
+# moja_tupla2 = moja_tupla
+# print(moja_tupla2)
+
+# lista=list(moja_tupla)
+# lista.append(8)
+# moja_tupla=tuple(lista)
+# print(moja_tupla)
+
+moja_tupla = (3,4,5)
+moja_tupla = moja_tupla + (8,)
+print(moja_tupla)
+
+
+x, y = 3,4
+x, y = y, x
+print(x,y)
